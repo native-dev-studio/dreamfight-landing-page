3868fbc30e73137079e5be0cbcf2deb82df96297
Kel Kanhirun <kel@native.dev>
2022-05-15 23:47:42 -0400

    feat: adds hero image

--- dreamfight/src/pages/index.tsx
+++ dreamfight/src/pages/index.tsx
@@ -1,12 +1,14 @@
 import * as React from "react"
+
 import DreamFightLogo from '../components/DreamFightLogo';
+import hero from "../images/hero.png";
 
 const IndexPage = () => {
   return (
     <div className="relative bg-black overflow-hidden">
       <div className="max-w-7xl mx-auto">
         <div className="relative z-10 pb-8 bg-black sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
-          <main className="mx-auto max-w-7xl px-10">
+          <main className="mx-auto px-10">
             <div className="text-left">
               <div className='my-10'>
                 <DreamFightLogo />
@@ -14,7 +16,7 @@ const IndexPage = () => {
               <h1 className="mt-16 mb-8 text-white text-6xl tracking-tight font-extrabold">
                 Get in the game
               </h1>
-              <p className="mt-3 text-white sm:mt-5 sm:text-lg md:mt-5 md:text-xl lg:mx-0 text-white">
+              <p className="my-3 text-white sm:mt-5 sm:text-lg md:mt-5 md:text-xl lg:mx-0 text-white">
                 DreamFight is a new sports game augmented over live broadcast that channels the kid in all of us. Think NBA Jam. NFL Blitz. We're bringing back the magic of sports and gaming.
               </p>
               <div className="mt-5 sm:mt-8 sm:flex sm:justify-center justify-start">
@@ -32,6 +34,9 @@ const IndexPage = () => {
               </div>
             </div>
           </main>
+        </div>
+        <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+          <img className="w-full object-cover" src={hero} alt="" />
         </div>
       </div>
     </div>
