6de7081802fdd092f68617e42b523f9a20d2e4d2
Kel Kanhirun <kel@native.dev>
2022-05-16 13:40:36 -0400

    task: configures site manifest

--- dreamfight/gatsby-config.ts
+++ dreamfight/gatsby-config.ts
@@ -6,6 +6,19 @@ const config: GatsbyConfig = {
   },
   plugins: [
     'gatsby-plugin-postcss',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "DreamFight",
+        short_name: "DreamFight",
+        start_url: "/",
+        icon: "src/images/icon.png",
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,
+      },
+    }
   ],
 }
 
