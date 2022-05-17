55f6603b44134f4654f7e3ede80aa70eae4865fe
Kel Kanhirun <kel@native.dev>
2022-05-17 07:52:59 -0400

    fix: missing gatsby-plugin-image config; fixes DEV-87

--- dreamfight/gatsby-config.ts
+++ dreamfight/gatsby-config.ts
@@ -26,6 +26,7 @@ const config: GatsbyConfig = {
         crossOrigin: `use-credentials`,
       },
     },
+    'gatsby-plugin-image',
     'gatsby-plugin-postcss',
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
