f6e56352429d6bfcfc2b869fb40bd94932a3b1f0
Kel Kanhirun <kel@native.dev>
2022-05-22 22:15:17 -0400

    feat: styles

--- dreamfight/src/pages/index.tsx
+++ dreamfight/src/pages/index.tsx
@@ -23,8 +23,8 @@ const IndexPage = () => {
   const image = getImage(data.hero);
 
   return (
-    <section className="flex justify-center 
-      lg:flex-row-reverse sm:flex-col-reverse flex-col-reverse"
+    <section className="flex justify-center
+      md:flex-row-reverse flex-col-reverse"
     >
       { /* @ts-ignore: Type 'undefined' is not assignable */ }
       <GatsbyImage image={image} imgStyle={{ objectFit: 'contain', objectPosition: 'left top' }} className="h-screen" alt=""/>
@@ -33,20 +33,32 @@ const IndexPage = () => {
           <div className='my-10'>
             <DreamFightLogo />
           </div>
-          <h1 className="italic mt-16 2xl:mt-16 mb-8 text-white text-6xl md:text-7xl 2xl:text-8xl tracking-tight font-extrabold">
+          <h1 className="
+            mt-4 sm:mt-8 2xl:mt-16 
+            mb-8 
+            italic text-white tracking-tight font-extrabold
+          ">
             Get in the game
           </h1>
-          <p className="my-3 text-white sm:mt-5 text-lg md:text-2xl md:mt-5 md:text-xl lg:mx-0 text-white">
+          <p className="
+            my-3 text-white sm:mt-5 md:mt-5 lg:mx-0
+          ">
             DreamFight is a new sports game augmented over live broadcast. We're bringing back the magic of sports and gaming.
           </p>
           <div className="mt-5 sm:mt-8 sm:flex sm:justify-center justify-start">
             <div className="w-full">
-              <div className='my-3 text-white text-lg font-bold'>
+              <div className='
+                text-md
+                my-3 text-white font-bold
+              '>
                 Sign up for early access
               </div>
               <div className='flex flex-col sm:flex-row sm:space-x-4 w-3xl font-large'>
                 <input type="text" className="py-3 mb-3 sm:mb-0 focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded text-lg px-3" placeholder="Enter your email"/>
-                <button className='px-8 py-3 text-lg whitespace-nowrap text-white bg-purple rounded'>
+                <button className='
+                  px-8 py-3 whitespace-nowrap text-white bg-purple rounded
+                  text-lg sm:text-md
+                '>
                   Sign up
                 </button>
               </div>
