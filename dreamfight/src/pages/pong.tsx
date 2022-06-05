74d3837e959d0dffc68e264ca8ad7500de5acf45
Kel Kanhirun <kel@native.dev>
2022-06-04 22:23:20 -0400

    task: useRef to access <video> DOM element

--- dreamfight/src/pages/pong.tsx
+++ dreamfight/src/pages/pong.tsx
@@ -7,33 +7,22 @@ import * as Pixi from "pixi.js";
 import Hls from 'hls.js';
 
 const PongPage = () => {
+  const videoRef = React.useRef<HTMLVideoElement>(null);
+  const hls = new Hls();
+
   React.useEffect(() => {
-    var video = document.getElementById('video') as HTMLMediaElement;
-    var videoSrc = 'https://547f72e6652371c3.mediapackage.us-east-1.amazonaws.com/out/v1/28c261ccdfc94e1ca1925a4401ea4e48/index.m3u8';
+    const videoTag = videoRef.current as HTMLVideoElement;
+    const src = 'https://547f72e6652371c3.mediapackage.us-east-1.amazonaws.com/out/v1/28c261ccdfc94e1ca1925a4401ea4e48/index.m3u8';
     if (Hls.isSupported()) {
-      var hls = new Hls();
-      hls.loadSource(videoSrc);
-      hls.attachMedia(video);
-    } 
-    // HLS.js is not supported on platforms that do not have Media Source
-    // Extensions (MSE) enabled.
-    //
-    // When the browser has built-in HLS support (check using `canPlayType`),
-    // we can provide an HLS manifest (i.e. .m3u8 URL) directly to the video
-    // element through the `src` property. This is using the built-in support
-    // of the plain video element, without using HLS.js.
-    //
-    // Note: it would be more normal to wait on the 'canplay' event below however
-    // on Safari (where you are most likely to find built-in HLS support) the
-    // video.src URL must be on the user-driven white-list before a 'canplay'
-    // event will be emitted; the last video event that can be reliably
-    // listened-for when the URL is not on the white-list is 'loadedmetadata'.
-    else if (video.canPlayType('application/vnd.apple.mpegurl')) {
-      video.src = videoSrc;
+      hls.loadSource(src);
+      hls.attachMedia(videoTag);
+    } else if (videoTag.canPlayType('application/vnd.apple.mpegurl')) {
+      videoTag.src = src;
     }
   });
+
   return (
-    <video id="video" autoPlay />
+    <video ref={videoRef} autoPlay />
   );
 }
 
