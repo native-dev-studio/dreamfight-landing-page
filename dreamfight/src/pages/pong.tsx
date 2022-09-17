fa55f3b84376d512f73cc1805234ad6264267b95
Kel Kanhirun <kel@native.dev>
2022-09-17 16:04:00 -0400

    feat: syncs predictions with pixijs loop

--- dreamfight/src/pages/pong.tsx
+++ dreamfight/src/pages/pong.tsx
@@ -11,80 +11,51 @@ import '@tensorflow/tfjs';
 const PongPage = () => {
   const videoRef = React.useRef<HTMLVideoElement>(null);
   const hls = new Hls();
-  /// Load video stream by transmuxing to mp4 fragments
-  const videoTag = videoRef.current as HTMLVideoElement;
-
-  /// Stubbed model so we can play camera
-  let model: any = false;
-  const predictWebcam = () => {
-    console.log('[predictWebcam]');
-    model.detect(videoRef.current).then((preds: any) => {
-      console.log('[predictWebcam.predict]: ', preds);
-    })
-    window.requestAnimationFrame(predictWebcam);
-  }
-
-  const enableWebcam = (event: Event) => {
-    cocoSSD.load().then(loadedModel => {
-      console.log('[enableWebcam.cocoSSD]', loadedModel)
-      model = loadedModel;
-      // getUsermedia parameters to force video but not audio.
-      const constraints = {
-        video: true
-      };
-
-      // Activate the webcam stream.
-      navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
-        videoRef.current.srcObject = stream;
-        /* videoTag.src = URL.createObjectURL(stream); */
-        videoRef.current.addEventListener('loadeddata', predictWebcam);
-      });
-    }, (err) => console.error(err));
-  }
 
   React.useEffect(() => {
-    /* const src = 'https://63050ee307b58b8f.mediapackage.us-east-1.amazonaws.com/out/v1/337bba2ce017459383a6a1781491c443/index.m3u8'; */
-    /* if (Hls.isSupported()) { */
-    /*   hls.loadSource(src); */
-    /*   hls.attachMedia(videoTag); */
-    /* } else if (videoTag.canPlayType('application/vnd.apple.mpegurl')) { */
-    /*   videoTag.src = src; */
-    /* } */
-    /* const url = 'wss://3ds4aci6dc.execute-api.us-east-1.amazonaws.com/test' */
-    /* const ws =  new WebSocket(url) */
-    /* ws.onopen = () => { */
-    /*   console.log('[WebSocket.onopen]'); */
-    /* } */
-    /* ws.onmessage = (evt) => { */
-    /*   console.log('[WebSocket.onmessage]', evt); */
-    /* } */
-    /* ws.onerror = (err) => { */
-    /*   console.log('[WebSocket.onerror]', err); */
-    /* } */
+    /// Load video stream by transmuxing to mp4 fragments
+    const videoTag = videoRef.current as HTMLVideoElement;
 
-    /// Play video stream from PixiJs
-    /* const node = document.getElementById('broadcast'); */
-    /* node?.appendChild(app.view); */
-    /* const texture = Pixi.Texture.from(videoTag); */
-    /* const sprite = Pixi.Sprite.from(texture); */
-    /* app.stage.addChild(sprite); */
+   // TODO: Size me properly
+   const app = new Pixi.Application({
+     width: 1280,
+     height: 720,
+   });
 
-    /* const circle = new Pixi.Graphics() */
-    /*   .beginFill(0xDC2626) */
-    /*   .drawCircle(10, 10, 10) */
-    /*   .endFill() */
-    /* app.stage.addChild(circle); */
+   cocoSSD.load().then(model => {
+     console.log('[cocoSSD.load]');
 
-    /* return () => { */
-    /*   node?.removeChild(app.view); */
-    /*   texture.baseTexture.destroy(); */
-    /* } */
+     /// Video stream
+     const src = 'https://63050ee307b58b8f.mediapackage.us-east-1.amazonaws.com/out/v1/337bba2ce017459383a6a1781491c443/index.m3u8';
+     if (Hls.isSupported()) {
+       hls.loadSource(src);
+       hls.attachMedia(videoTag);
+     } else if (videoTag.canPlayType('application/vnd.apple.mpegurl')) {
+       videoTag.src = src;
+     }
+
+     /// Process video onto canvas
+     const node = document.getElementById('broadcast');
+     node?.appendChild(app.view);
+     const videoTexture = Pixi.Texture.from(videoTag);
+     const videoSprite = Pixi.Sprite.from(videoTexture);
+     app.stage.addChild(videoSprite);
+
+     /// Augmentation stream, turn it into object tracking loop
+     /// TODO: Replace with custom render loop to keep predictions and video in sync
+     app.ticker.add(delta => {
+       // Remove previous image
+       model.detect(videoTag).then((preds: any) => {
+         // Set new image
+         console.log(preds);
+       })
+     });
+   });
   }, []);
 
   return (
     <div id='broadcast'>
-      <button onClick={enableWebcam}>Enable webcam</button>
-      <video ref={videoRef} autoPlay controls muted />
+      <video ref={videoRef} autoPlay controls muted style={{ display: 'none' }}/>
     </div>
   );
 }
