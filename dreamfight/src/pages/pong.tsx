1775a7ac68d7459c40ee2dea3a8c886299395e5a
Kel Kanhirun <kel@native.dev>
2022-09-17 12:06:05 -0400

    feat: web camera working

--- dreamfight/src/pages/pong.tsx
+++ dreamfight/src/pages/pong.tsx
@@ -9,56 +9,80 @@ import * as Pixi from "pixi.js";
 const PongPage = () => {
   const videoRef = React.useRef<HTMLVideoElement>(null);
   const hls = new Hls();
-  // TODO: Size me properly
-  const app = new Pixi.Application({
-    width: 1280,
-    height: 720,
-  });
+  /// Load video stream by transmuxing to mp4 fragments
+  const videoTag = videoRef.current as HTMLVideoElement;
+
+  /// Stubbed model so we can play camera
+  const model = true;
+
+  const predictWebcam = (event) => {
+    console.log('[predictWebcam]');
+  }
+
+  const enableWebcam = (event) => {
+    console.log('[enableWebcam]');
+    // Only continue if the COCO-SSD has finished loading.
+    if (!model) {
+      console.log('[enableWebcam]: model not defined');
+      return;
+    }
+
+    // getUsermedia parameters to force video but not audio.
+    const constraints = {
+      video: true
+    };
+
+    // Activate the webcam stream.
+    navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+      videoRef.current.srcObject = stream;
+      /* videoTag.src = URL.createObjectURL(stream); */
+      videoRef.current.addEventListener('loadeddata', predictWebcam);
+    });
+  }
 
   React.useEffect(() => {
-    /// Load video stream by transmuxing to mp4 fragments
-    const videoTag = videoRef.current as HTMLVideoElement;
-    const src = 'https://63050ee307b58b8f.mediapackage.us-east-1.amazonaws.com/out/v1/337bba2ce017459383a6a1781491c443/index.m3u8';
-    if (Hls.isSupported()) {
-      hls.loadSource(src);
-      hls.attachMedia(videoTag);
-    } else if (videoTag.canPlayType('application/vnd.apple.mpegurl')) {
-      videoTag.src = src;
-    }
-    const url = 'wss://3ds4aci6dc.execute-api.us-east-1.amazonaws.com/test'
-    const ws =  new WebSocket(url)
-    ws.onopen = () => {
-      console.log('[WebSocket.onopen]');
-    }
-    ws.onmessage = (evt) => {
-      console.log('[WebSocket.onmessage]', evt);
-    }
-    ws.onerror = (err) => {
-      console.log('[WebSocket.onerror]', err);
-    }
+    /* const src = 'https://63050ee307b58b8f.mediapackage.us-east-1.amazonaws.com/out/v1/337bba2ce017459383a6a1781491c443/index.m3u8'; */
+    /* if (Hls.isSupported()) { */
+    /*   hls.loadSource(src); */
+    /*   hls.attachMedia(videoTag); */
+    /* } else if (videoTag.canPlayType('application/vnd.apple.mpegurl')) { */
+    /*   videoTag.src = src; */
+    /* } */
+    /* const url = 'wss://3ds4aci6dc.execute-api.us-east-1.amazonaws.com/test' */
+    /* const ws =  new WebSocket(url) */
+    /* ws.onopen = () => { */
+    /*   console.log('[WebSocket.onopen]'); */
+    /* } */
+    /* ws.onmessage = (evt) => { */
+    /*   console.log('[WebSocket.onmessage]', evt); */
+    /* } */
+    /* ws.onerror = (err) => { */
+    /*   console.log('[WebSocket.onerror]', err); */
+    /* } */
 
     /// Play video stream from PixiJs
-    const node = document.getElementById('broadcast');
-    node?.appendChild(app.view);
-    const texture = Pixi.Texture.from(videoTag);
-    const sprite = Pixi.Sprite.from(texture);
-    app.stage.addChild(sprite);
+    /* const node = document.getElementById('broadcast'); */
+    /* node?.appendChild(app.view); */
+    /* const texture = Pixi.Texture.from(videoTag); */
+    /* const sprite = Pixi.Sprite.from(texture); */
+    /* app.stage.addChild(sprite); */
 
-    const circle = new Pixi.Graphics()
-      .beginFill(0xDC2626)
-      .drawCircle(10, 10, 10)
-      .endFill()
-    app.stage.addChild(circle);
+    /* const circle = new Pixi.Graphics() */
+    /*   .beginFill(0xDC2626) */
+    /*   .drawCircle(10, 10, 10) */
+    /*   .endFill() */
+    /* app.stage.addChild(circle); */
 
-    return () => {
-      node?.removeChild(app.view);
-      texture.baseTexture.destroy();
-    }
-  });
+    /* return () => { */
+    /*   node?.removeChild(app.view); */
+    /*   texture.baseTexture.destroy(); */
+    /* } */
+  }, []);
 
   return (
     <div id='broadcast'>
-      <video ref={videoRef} autoPlay controls muted style={{ display: 'none' }} />
+      <button onClick={enableWebcam}>Enable webcam</button>
+      <video ref={videoRef} autoPlay controls muted />
     </div>
   );
 }
