e2a44cf9eae824e79032dd0122bb1f0a974c6ca4
Cesar Figueroa <cesar.figueroa@me.com>
2022-09-28 16:58:12 -0400

    Add UI, paddle mouse events

--- dreamfight/src/pages/pong.tsx
+++ dreamfight/src/pages/pong.tsx
@@ -5,21 +5,9 @@ import * as Pixi from "pixi.js";
 import * as cocoSSD from "@tensorflow-models/coco-ssd";
 import "@tensorflow/tfjs";
 import chunks from "../data/data.json";
-import * as Stream from "rxjs";
-
-const emit = (coords: any) => {
-  if (coords.length === 0) {
-    return;
-  }
-  setTimeout(() => {
-    const coord = coords.shift();
-    const event = {
-      TennisBall: coord,
-    };
-    console.log(event);
-    emit(coords);
-  }, 66.6666666667);
-};
+import * as Observable from "rxjs";
+import ease, { presets } from "rx-ease";
+import playIcon from "../images/play.svg";
 
 // Stub video object (to be replaced with some introspected data)
 const VIDEO = {
@@ -27,6 +15,21 @@ const VIDEO = {
   height: 720,
   fps: 60,
 };
+
+const PADDLE_HEIGHT = 100;
+
+/*
+  EVENTS
+    - Tennis ball
+      - x, y, w, h
+        - 7 digit precision for fractions (0.8104252)
+
+    - Service Shot (no params; represents hit shot in the game)
+      - (flash red circle)
+      - Auto returns a shot
+      - status (open|settled)
+        ask
+*/
 
 const PongPage = () => {
   const videoRef = React.useRef<HTMLVideoElement>(null);
@@ -41,8 +44,8 @@ const PongPage = () => {
   React.useEffect(() => {
     const videoTag = videoRef.current as HTMLVideoElement;
 
-    const mlModel$ = Stream.from(cocoSSD.load());
-    const fragChanged$ = Stream.fromEventPattern<FragChangedData>(
+    const mlModel$ = Observable.from(cocoSSD.load());
+    const fragChanged$ = Observable.fromEventPattern<FragChangedData>(
       (handler) => {
         hls.on(Hls.Events.FRAG_CHANGED, (evt, data) => {
           handler(data);
@@ -53,20 +56,7 @@ const PongPage = () => {
       }
     );
 
-    /*
-      EVENTS
-        - Tennis ball
-          - x, y, w, h
-            - 7 digit precision for fractions (0.8104252)
-
-        - Service Shot (no params; represents hit shot in the game)
-          - (flash red circle)
-          - Auto returns a shot
-          - status (open|settled)
-            ask
-    */
-
-    /// Video stream
+    // Video stream
     const src =
       "https://63050ee307b58b8f.mediapackage.us-east-1.amazonaws.com/out/v1/337bba2ce017459383a6a1781491c443/index.m3u8";
     if (Hls.isSupported()) {
@@ -83,91 +73,200 @@ const PongPage = () => {
     const videoSprite = Pixi.Sprite.from(videoTexture);
     app.stage.addChild(videoSprite);
 
-    const PADDLE_KEYS = { left: 37, right: 39 };
-    // const playerEvents$ = Stream.fromEvent(document, "keydown", (event) => {
-    //   switch (event.keyCode) {
-    //     case PADDLE_KEYS.left:
-    //       return -1;
-    //     case PADDLE_KEYS.right:
-    //       return 1;
-    //     default:
-    //       return 0;
-    //   }
+    const tennis = new Pixi.Graphics()
+      .beginFill(0xff0000)
+      .drawCircle(16, 16, 16)
+      .endFill();
+
+    const paddleLeft = new Pixi.Graphics()
+      .beginFill(0xffffff)
+      .drawRect(0, 0, 50, 200)
+      .endFill();
+
+    const paddleRight = new Pixi.Graphics()
+      .beginFill(0xffffff)
+      .drawRect(VIDEO.width - 50, 0, 50, 200)
+      .endFill();
+
+    app.stage.addChild(tennis);
+    app.stage.addChild(paddleLeft);
+    app.stage.addChild(paddleRight);
+
+    // const progress$ = Observable.interval(250).pipe(
+    //   Observable.startWith(0),
+    //   Observable.map((i) =>
+    //     i % 3 === 0
+    //       ? { x: 400, y: 400 }
+    //       : i % 2 === 0
+    //       ? { x: 800, y: 200 }
+    //       : { x: 0, y: 0 }
+    //   ),
+    //   ease({
+    //     x: [120, 18],
+    //     y: [120, 18],
+    //   })
+    // ).subscribe(({ x, y }) => {
+    //   tennis.position.x = x;
+    //   tennis.position.y = y;
     // });
 
-    hls.on(Hls.Events.FRAG_CHANGED, (evt, data) => {
-      const sn = data.frag.sn;
-      const secondsPerChunk = 6;
-      const videoLengthInMinutes = 10;
-      const secondsInMinutes = 60;
-      const totalChunks =
-        (videoLengthInMinutes * secondsInMinutes) / secondsPerChunk;
-      const chunkId = (sn as number) % totalChunks;
-      const tennisBallCoordinates = chunks[chunkId];
+    const mouse$ = Observable.fromEvent<PointerEvent>(
+      document,
+      "pointermove"
+    ).pipe(
+      Observable.map((e) => {
+        console.log("!", e.clientY / VIDEO.height);
+        return clamp((e.clientY - PADDLE_HEIGHT) / VIDEO.height, 0, 1);
+      })
+    );
 
-      console.log("[Hls.FRAG_CHANGED]", chunkId);
-      emit(tennisBallCoordinates);
+    const arrows$ = Observable.merge(
+      Observable.fromEvent<KeyboardEvent>(document, "keydown").pipe(
+        Observable.filter((e) => e.code === "ArrowUp"),
+        Observable.map(() => -0.1) // 10% of the screen
+      ),
+      Observable.fromEvent<KeyboardEvent>(document, "keydown").pipe(
+        Observable.filter((e) => e.code === "ArrowDown"),
+        Observable.map(() => 0.1)
+      )
+    ).pipe(
+      Observable.scan((a, b) => clamp(a + b, 0, 1)),
+      ease(500, 100)
+    );
+
+    Observable.merge(arrows$, mouse$).subscribe((v) => {
+      const y = (VIDEO.height - paddleLeft.height) * v; /* / 100 */
+
+      console.log("up", v, y);
+      paddleLeft.position.y = y;
     });
 
-    const mlEvents$ = new Stream.Subject();
+    fragChanged$
+      .pipe(
+        Observable.mergeMap((frag) => {
+          console.log("fragChanged", frag);
+          const sn = frag.frag.sn;
+          const secondsPerChunk = 6;
+          const videoLengthInMinutes = 10;
+          const secondsInMinutes = 60;
+          const totalChunks =
+            (videoLengthInMinutes * secondsInMinutes) / secondsPerChunk;
+          const chunkId = (sn as number) % totalChunks;
 
-    const videoPlayState$ = Stream.fromEvent(videoTag, "play");
-    videoPlayState$.subscribe({
-      next: (s) => console.log("videoPlayState$", s),
-    });
+          return Observable.from(chunks[chunkId]).pipe(
+            Observable.concatMap((chunk) => {
+              return chunk
+                ? Observable.of({ x: chunk[0], y: chunk[1] }).pipe(
+                    Observable.delay(1000 / 15)
+                  )
+                : Observable.EMPTY;
+            }),
+            ease({
+              x: [120, 18],
+              y: [120, 18],
+            })
+          );
+        })
+      )
+      .subscribe((positions) => {
+        if (positions) {
+          console.log("positions", [
+            positions.x * VIDEO.width,
+            positions.y * VIDEO.height,
+          ]);
 
-    const videoPauseState$ = Stream.fromEvent(videoTag, "pause");
-    videoPauseState$.subscribe({
-      next: (s) => console.log("videoPauseState$", s),
-    });
+          tennis.position.x = positions.x * VIDEO.width;
+          tennis.position.y = positions.y * VIDEO.height;
+        }
+      });
+
+    // const mlEvents$ = new Stream.Subject();
+
+    const videoPlayState$ = Observable.fromEvent(videoTag, "play");
+    const videoPauseState$ = Observable.fromEvent(videoTag, "pause");
+
+    // videoPlayState$.subscribe({
+    //   next: (s) => console.log("videoPlayState$", s),
+    // });
+
+    // videoPauseState$.subscribe({
+    //   next: (s) => console.log("videoPauseState$", s),
+    // });
 
     const TICKER_INTERVAL = 1000 / 15; // FPS
 
     // Start a loop
-    Stream.interval(TICKER_INTERVAL)
-      .pipe(
-        // Aggregate the ML model and fragment changes and wait until they've all loaded
-        Stream.withLatestFrom(
-          mlModel$,
-          fragChanged$
-          // playerEvents$ /* serverSideEvents$ */
-        ),
+    Observable.interval(TICKER_INTERVAL).pipe(
+      // Aggregate the ML model and fragment changes and wait until they've all loaded
+      Observable.withLatestFrom(mlModel$, fragChanged$),
 
-        // Accept events when the video is playing, and filter them out when it's paused
-        Stream.windowToggle(videoPlayState$, () => videoPauseState$),
-        Stream.mergeAll(),
+      // Accept events when the video is playing, and filter them out when it's paused
+      Observable.windowToggle(videoPlayState$, () => videoPauseState$),
+      Observable.mergeAll(),
 
-        // And only allow up to the FPS interval events/sec (e.g. 16.67ms for 60fps)
-        Stream.sampleTime(TICKER_INTERVAL)
-      )
-      .subscribe(function update([_t, coco, frag /* playerEvents */]) {
-        console.log("update here!!!", { _t, coco, frag /* playerEvents */ });
+      // And only allow up to the FPS interval events/sec (e.g. 16.67ms for 60fps)
+      Observable.sampleTime(TICKER_INTERVAL),
+      Observable.observeOn(Observable.animationFrameScheduler)
+    );
+    // .subscribe(function update([_t, coco, frag /* playerEvents */]) {
+    //   console.log("update here!!!", { _t, coco, frag /* playerEvents */ });
 
-        coco.detect(videoTag).then((preds) => {
-          const { bbox, score } = preds[0];
-          const [x, y, w, h] = bbox;
+    //   coco.detect(videoTag).then((preds) => {
+    //     const { bbox, score } = preds[0];
+    //     const [x, y, w, h] = bbox;
 
-          // const rectGroup = new Pixi.UniformGroup();
-          const rect: Pixi.Graphics = new Pixi.Graphics()
-            .beginFill(0x0000ff)
-            .drawRect(10, 10, 10, 10)
-            .endFill();
+    //     // const rectGroup = new Pixi.UniformGroup();
+    //     const rect: Pixi.Graphics = new Pixi.Graphics()
+    //       .beginFill(0x0000ff)
+    //       .drawRect(10, 10, 10, 10)
+    //       .endFill();
 
-          rect.position.x = x;
-          rect.position.y = y;
-          app.stage.addChild(rect);
-          app.renderer.render(app.stage);
+    //     rect.position.x = x;
+    //     rect.position.y = y;
+    //     app.stage.addChild(rect);
+    //     // app.renderer.render(app.stage);
 
-          mlEvents$.next({
-            // give augmented video buffer of 5-10 sec to be behind live video,
-          });
-        });
-      });
+    //     // mlEvents$.next({
+    //     //   // give augmented video buffer of 5-10 sec to be behind live video,
+    //     //   type: "rect",
+    //     //   params: { width: 200, height: 200 },
+    //     //   x: 100,
+    //     //   y: 100,
+    //     // });
+    //   });
+    // });
+
+    return () => {
+      app.stage.removeChildren();
+    };
   }, []);
 
   return (
-    <div id="broadcast">
+    <div
+      id="broadcast"
+      style={{
+        height: "100vh",
+        position: "relative",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
       <button
+        style={{
+          position: "absolute",
+          background: "#FFF",
+          borderRadius: 80,
+          width: 80,
+          height: 80,
+          top: "50%",
+          marginTop: -40,
+          left: "50%",
+          marginLeft: -40,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
         onClick={() => {
           if (videoRef.current?.paused) {
             videoRef.current?.play();
@@ -176,7 +275,7 @@ const PongPage = () => {
           }
         }}
       >
-        Play/Pause
+        <img src={playIcon} alt="" />
       </button>
 
       <video
@@ -193,3 +292,11 @@ const PongPage = () => {
 };
 
 export default PongPage;
+
+function clamp(
+  val: number,
+  min = Number.NEGATIVE_INFINITY,
+  max = Number.POSITIVE_INFINITY
+) {
+  return Math.max(min, Math.min(max, val));
+}
