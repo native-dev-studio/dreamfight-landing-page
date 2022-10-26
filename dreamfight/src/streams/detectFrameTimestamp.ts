2337971c1c80870037e37e3b060a376020cda14a
Kel Kanhirun <kel@native.dev>
2022-10-26 18:59:28 +0300

    feat: adds stream op for debugging frame count

---
+++ dreamfight/src/streams/detectFrameTimestamp.ts
@@ -0,0 +1,16 @@
+import { Observable, Subject } from "rxjs";
+import { map } from "rxjs/operators";
+import { pipe as _ } from "fp-ts/lib/function";
+import { generatePlayheadIndex } from './mockUtils';
+
+export function detectFrameTimestamp$(
+  imageSource$: Subject<ImageData>
+): Observable<number> {
+  return _(
+    imageSource$,
+    map((imdata: ImageData) => {
+      const playheadIndex = generatePlayheadIndex(imdata);
+      return playheadIndex;
+    })
+  )
+}
