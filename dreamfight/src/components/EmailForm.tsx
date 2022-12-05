0512f2d64c659c6218dbdde43eb72a00bb821f2e
Cesar Figueroa <cesar.figueroa@me.com>
2022-12-05 17:01:22 -0500

    feat: Stack sign up fields on mobile, rename button

--- dreamfight/src/components/EmailForm.tsx
+++ dreamfight/src/components/EmailForm.tsx
@@ -10,10 +10,9 @@ export function EmailForm() {
             <form action="https://dreamfight.us12.list-manage.com/subscribe/post?u=a735595d65f2b9f11427afb2b&amp;id=1f4100463b&amp;f_id=00fdb4e0f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_self">
               <div id="mc_embed_signup_scroll">
                 <div class="mc-field-group">
-                  <label for="mce-EMAIL">Get your pass!</label>
                   <div class="mc-field-with-button">
                     <input type="email" placeholder="Enter email" value="" name="EMAIL" class="required email" id="mce-EMAIL" required />
-                    <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button" />
+                    <input type="submit" value="Sign Up" name="subscribe" id="mc-embedded-subscribe" class="button" />
                   </div>
                 </div>
 
