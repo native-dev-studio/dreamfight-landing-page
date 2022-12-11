43b7869bab76ed1e05dc8f838785c32f804ec801
Kel Kanhirun <kel@native.dev>
2022-12-11 14:46:35 -0500

    feat: adds social icons to footer, resolves DF-558

--- dreamfight/src/components/Footer.tsx
+++ dreamfight/src/components/Footer.tsx
@@ -1,11 +1,31 @@
 import * as React from "react";
 import badgeSrc from "../images/badge.png";
+import TwitterIcon from '../images/icon-twitter.svg';
+import TwitchIcon from '../images/icon-twitch.svg';
+import YoutubeIcon from '../images/icon-youtube.svg';
+import InstagramIcon from '../images/icon-instagram.svg';
 
 export function Footer() {
   return (
-    <footer className="footer">
-      <img src={badgeSrc} alt="DF badge" className="footer-badge" />
-      <p className="content">© 2022 DreamFight Inc. All rights reserved.</p>
+    <footer className="flex flex-col gap-5 sm:flex-row items-center justify-between mt-20 mb-10 mx-10">
+      <div className='flex items-center'>
+        <img src={badgeSrc} alt="DF badge" className="footer-badge" />
+        <p className="content">© 2022 DreamFight Inc. All rights reserved.</p>
+      </div>
+      <div className='flex gap-5'>
+        <a href='https://twitter.com/dreamfightgame' target='_blank'>
+          <TwitterIcon className='w-5 sm:w-10' />
+        </a>
+        <a href='https://www.twitch.tv/dreamfightgame' target='_blank'>
+          <TwitchIcon className='w-5 sm:w-10' />
+        </a>
+        <a href='https://www.youtube.com/@dreamfightgame/' target='_blank'>
+          <YoutubeIcon className='w-5 sm:w-10' />
+        </a>
+        <a href='https://www.instagram.com/dreamfightgame/' target='_blank'>
+          <InstagramIcon className='w-5 sm:w-10' />
+        </a>
+      </div>
     </footer>
   );
 }
