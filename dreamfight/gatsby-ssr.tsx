a8db786b9d64f7f9a3a5b6d6530427eefa6e597f
Kel Kanhirun <kel@native.dev>
2022-12-21 10:43:19 -0500

    feat: adds og metadata, resolves DF-553

--- dreamfight/gatsby-ssr.tsx
+++ dreamfight/gatsby-ssr.tsx
@@ -3,10 +3,15 @@ import type { GatsbySSR } from "gatsby"
 
 export const onPreRenderHTML: GatsbySSR["onPreRenderHTML"] = ({ getHeadComponents, replaceHeadComponents }) => {
   const headComponents = getHeadComponents();
-  const key = headComponents.length;
+
+  const tagline     = "DreamFight | A new era in gaming";
+  const description = "DreamFight is a radically new strategy game played on top of live sports broadcast. Play as your favorite champions and fight!";
 
   replaceHeadComponents([
     ...headComponents,
-    <title key={key}>DreamFight</title>
+    <title key={0}>DreamFight</title>,
+    <meta  key={1} property="og:title" content={tagline} />,
+    <meta  key={2} property="og:type" content="website" />,
+    <meta  key={3} property="og:description" content={description} />,
   ])
 }
